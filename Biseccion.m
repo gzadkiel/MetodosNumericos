d3cec7f9f55f63db2e89450a0017ae4a9ed8fb7d
Gerónimo <tickimarti@hotmail.com>
2023-03-26 16:01:32 -0300

    Bisection Method

--- Biseccion.m
+++ Biseccion.m
@@ -1,28 +1,56 @@
-f = input('funcion: ','s');
-F = inline(f); 
-a = input('lim. inferior del int: '); 
-b = input('lim. superior del int: '); 
-tolx = input('tol. en x: '); 
-toly = input('tol. en y: '); 
-xant = 0; 
-ey = ex = 999; 
-i = 0; 
-while (ex > tolx) && (ey > toly) 
-  i = i+1; 
-  xs = (a+b)/2 
-  ya = F(a);
-  yb = F(b); 
-  fxs = F(xs);
-  ex = abs(xs-xant) 
-  ey = abs(fxs) 
-  if fxs*ya < 0
-    b = xs; 
-  else
-    a = xs; 
-  end
-  xant = xs; 
+% Inputs:
+%  - F: Function handle of the function to be evaluated
+%  - a, b: Interval limits
+%  - tolx, toly: Tolerances for x and y, respectively
+%
+% Outputs:
+%  - x: The root of the function within the interval (a,b)
+%  - niter: Number of iterations needed to converge to the root
+%  - ex: Error in x
+%  - ey: Error in y
+%
+% Example usage:
+% f = @(x) x^3 - 2*x - 5;
+% a = 1;
+% b = 3;
+% tolx = 1e-6;
+% toly = 1e-6;
+% [x, niter, ex, ey] = bisection(f, a, b, tolx, toly);
+
+function [x, niter, ex, ey] = biseccion(F, a, b, tolx, toly)
+
+    % Initialize variables for loop
+    xant    = 0;
+    ey = ex = 999;
+    i       = 0;
+
+    % Iterate until both tolerances are met
+    while (ex > tolx) && (ey > toly)
+        i   = i + 1;               % Increment iteration counter
+        xs  = (a + b) / 2;         % Calculate interval midpoint
+        ya  = F(a);                % Evaluate function at interval endpoint a
+        yb  = F(b);                % Evaluate function at interval endpoint b
+        fxs = F(xs);               % Evaluate function at interval midpoint
+        ex  = abs(xs - xant);      % Calculate error in x
+        ey  = abs(fxs);            % Calculate error in y
+        if fxs*ya < 0              % Check if root is in left half of interval
+            b = xs;
+        else                       % Otherwise, root is in right half of interval
+            a = xs; 
+        end
+        xant = xs;                 % Update previous x value
+    end
+
+    % Output results
+    x       = xant;
+    niter   = i;
+    ex      = ex;
+    ey      = ey;
+
+    % Plot results 
+    fprintf('The root is: %f\n', x);
+    fprintf('Number of iterations: %d\n', niter);
+    fprintf('Error in x: %f\n', ex);
+    fprintf('Error in y: %f\n', ey);
+
 end
-res1 = ['la raiz es: ',num2str(xant)]; disp(res1) 
-res2 = ['cant. de interaciones: ',num2str(i)]; disp(res2) 
-res3 = ['error en x: ',num2str(ex)]; disp(res3) 
-res4 = ['error en y: ',num2str(ey)]; disp(res4)
