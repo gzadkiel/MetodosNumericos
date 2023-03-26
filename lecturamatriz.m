6a12ad59038ea962d120767fd9e19b6e4db9bfe8
Gerónimo <tickimarti@hotmail.com>
2023-03-26 20:03:43 -0300

    Reads a matrix from user input

--- lecturamatriz.m
+++ lecturamatriz.m
@@ -1,15 +1,17 @@
-function mat=lecturamatriz
-  n=input('Ingrese el numero de filas de la matriz: ');
-  m=input('Ingrese el numero de columnas de la matriz: ');
-  A=zeros(n,m);
+% This function prompts the user to enter the dimensions and elements of a matrix,
+% and returns the resulting matrix. It is often used in conjunction with other
+% functions that require a matrix as input for calculations.
+
+function mat = lecturamatriz()
+  n = input('Ingrese el numero de filas de la matriz: ');
+  m = input('Ingrese el numero de columnas de la matriz: ');
+  A = zeros(n,m);
   for i=1:n 
-    disp('Elementos de la fila: ')
-    disp(i)
-    for j=1:m 
-     disp('Columna: ')
-     disp(j)
-     A(i,j)=input('... '); 
-    end
+      disp(['Elementos de la fila: ', num2str(i)]);
+      for j=1:m 
+          disp(['Columna: ', num2str(j)]);
+          A(i,j) = input('... '); 
+      end
   end
-mat=A 
-endfunction
+  mat = A; 
+end
