e9a6f90902a9553a455406c798fd606887c522d8
Gerónimo <tickimarti@hotmail.com>
2023-03-26 20:03:19 -0300

    Solves wave equation

--- EcuacionDeOnda.m
+++ EcuacionDeOnda.m
@@ -1,39 +1,56 @@
-#EcuacionDeOnda
+% Solves the wave equation numerically using the finite difference method
+%
+% Inputs:
+%  - h: spatial step size
+%  - k: time step size
+%  - cte: constant parameter in the wave equation
+%  - Ti: initial time
+%  - Tf: final time
+%  - Xi: initial position
+%  - Xf: final position
+%  - Bi: boundary value at the initial position
+%  - Bf: boundary value at the final position
+%  - F1: function handle for the initial position condition
+%  - F2: function handle for the initial velocity condition
+% Output:
+%  - O: matrix containing the solution of the wave equation at each time step and position
+%
+% Example usage:
+% O = EcuacionDeOnda(0.1, 0.01, 1, 0, 10, 0, 1, 0, 0, @(x) sin(x), @(x) cos(x))
 
-h = input('h (paso de x): '); 
-k = input('k (paso de t): '); 
-cte = input('constante: '); 
-r = (cte^2*k^2)/h^2; 
-Ti = input('t inicial: '); 
-Tf = input('t final: '); 
-Xi = input('x inicial: ');
-Xf = input('x final: '); 
-disp('Condiciones en los bordes: '); 
-Bi = input('borde incial: '); 
-Bf = input('borde final: '); 
-F1 = @(x) (); #posicion inicial 
-F2 = @(x) (); #velocidad inicial 
-
-vec_dist = Xi:h:Xf;
-vec_tiem = Ti:k:Tf; 
-
-for i=1:length(vec_dist)
-  O(1,i) = F1(vec_dist(i));    
-end
-for i=1:length(vec_tiem) 
-  O(i,1) = Bi; 
-  O(i,end) = Bf; 
-end
-O; 
-
-for j=2:length(vec_dist)-1 
-  O(2,j) = (r/2)*(O(1,j+1)+O(1,j-1)) + (1-r)*O(1,j) + k*F2(vec_dist(j));
-end
-
-for j=3:length(vec_tiem)
-  for i=2:length(vec_dist)-1
-  #for j=3:length(vec_tiem)-2 
-    O(j,i) = r*(O(j-1,i+1)+O(j-1,i-1)) + 2*(1-r)*O(j-1,i) - O(j-2,i); 
+function O = EcuacionDeOnda(h, k, cte, Ti, Tf, Xi, Xf, Bi, Bf, F1, F2)
+  
+  % Calculate the parameter r
+  r = (cte^2 * k^2) / h^2;
+  
+  % Create vectors for distance and time steps
+  vec_dist = Xi:h:Xf;
+  vec_tiem = Ti:k:Tf;
+  
+  % Initialize the solution matrix
+  O = zeros(length(vec_tiem), length(vec_dist));
+  
+  % Set the initial position values
+  for i = 1:length(vec_dist)
+      O(1,i) = F1(vec_dist(i));
   end
-end
-O 
+  
+  % Set the boundary values
+  for i = 1:length(vec_tiem)
+      O(i,1) = Bi;
+      O(i,end) = Bf;
+  end
+  
+  % Calculate the solution at time step 2 using the initial velocity condition
+  for j = 2:length(vec_dist)-1
+      O(2,j) = (r/2) * (O(1,j+1) + O(1,j-1)) + (1-r) * O(1,j) + k * F2(vec_dist(j));
+  end
+  
+  % Calculate the solution for the remaining time steps
+  for j = 3:length(vec_tiem)
+      for i = 2:length(vec_dist)-1
+          O(j,i) = r * (O(j-1,i+1) + O(j-1,i-1)) + 2 * (1-r) * O(j-1,i) - O(j-2,i);
+      end
+  end
+  end
+  
