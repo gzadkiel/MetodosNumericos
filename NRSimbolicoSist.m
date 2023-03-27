b0d89d6c9dde82aa84f1453a5881a3ba28c5377f
Gerónimo <tickimarti@hotmail.com>
2023-03-26 21:53:10 -0300

    NR for system of equations

--- NRSimbolicoSist.m
+++ NRSimbolicoSist.m
@@ -1,24 +1,42 @@
-clear
-pkg load symbolic 
-syms x
-syms y
-disp('ecuaciones con variables x,y') 
-f = input('ecuacion 1: '); 
-g = input('ecuacion 2: '); 
-J = jacobian([f,g],[x,y])
-sist = [f;g]; 
-x0 = lecturamatriz;
-#Iteraciones: 
-n = input('iteraciones: '); 
-#cota = input('cota de error: '); 
-#cota = input('cota del residuo: ');
-er = 999;  
-for i=1:n
-#while (er>cota) 
-  xsol = subs(x0-inv(J)*sist,[x;y],[x0(1,1);x0(2,1)]);
-  pasomedio = double(xsol);
-  er = norm(xsol-x0,1); 
-  x0 = pasomedio
+% Solves a system of equations using the Newton-Raphson method
+%
+% Inputs:
+%  - f: A string or symbolic expression representing the first equation of the system.
+%  - g: A string or symbolic expression representing the second equation of the system.
+%  - x0: A 2x1 matrix representing the initial guess for the values of x and y.
+%  - n: An integer representing the maximum number of iterations to be performed.
+%  - cota: Tolerance for error
+%
+% Outputs: 
+%
+%  - sol: A 2x1 matrix representing the solution of the system of equations.
+%  - iter: An integer representing the number of iterations performed.
+%  - err: Error
+%
+% Example usage:
+% syms x y
+% f = x^2 + y^2 - 1;
+% g = x + y - 2;
+% x0 = [0.5; 1.5];
+% n = 10;
+% cota = 1e-6;
+% [sol, iter, err] = NRSimbolicoSist(f, g, x0, n, cota)
+
+function [sol, iter, err] = NRSimbolicoSist(f, g, x0, n, cota)
+
+  syms x y
+  J = jacobian([f, g], [x, y]);
+  sist = [f; g];
+  er = 999;
+  iter = 0;
+  while (iter < n) && (er > cota)
+      iter = iter + 1;
+      xsol = subs(x0 - inv(J)*sist, [x; y], [x0(1); x0(2)]);
+      pasomedio = double(xsol);
+      er = norm(xsol - x0, 1);
+      x0 = pasomedio;
+  end
+  sol = x0;
+  err = er;
+  
 end
-disp('solucion: ')
-disp(x0)
