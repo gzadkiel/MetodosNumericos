566de46c658bcf437284e5e79219f507e193c4ce
Gerónimo <tickimarti@hotmail.com>
2023-03-27 13:23:22 -0300

    4th order Runge-Kutta

--- RungeKutta4toOrden.m
+++ RungeKutta4toOrden.m
@@ -1,26 +1,37 @@
-#RungeKutta4toOrden
-clear all; clc; close all; 
-h = input('Número de paso: '); 
-t0 = input('t inicial: ');  
-y0 = input('y inicial: '); 
-tf = input('Valor a especializar: '); 
-F = @(t,y) (); 
-tv = t0:h:tf 
-yv(1) = y0;  
+% This function implements the 4th order Runge-Kutta method for solving
+% ordinary differential equations.
+%
+% InputS:
+%  - h: Step size
+%  - t0: Initial value of t
+%  - y0: Initial value of y
+%  - tf: Final value of t to be calculated
+%  - F: Function handle to the function dy/dt = F(t, y)
+%
+% Outputs:
+%  - tv: Vector of time values
+%  - yv: Vector of corresponding solution values
+%  
+% Example usage:
+% Example usage
+% h = 0.1;
+% t0 = 0;
+% y0 = 1;
+% tf = 1;
+% F = @(t, y) -y*t; % example function dy/dt = -y*t
+% [tv, yv] = RungeKutta4toOrden(h, t0, y0, tf, F);
 
-for i=1:length(tv)-1
-  K1 = h*F(tv(i),yv(i));  
-  K2 = h*F(tv(i)+h/2,yv(i)+K1/2);
-  K3 = h*F(tv(i)+h/2,yv(i)+K2/2);
-  K4 = h*F(tv(i)+h,yv(i)+K3); 
-  yv(i+1) = yv(i) + (K1+2*K2+2*K3+K4)/6; 
+function [tv, yv] = RungeKutta4toOrden(h, t0, y0, tf, F)
+
+  tv = t0:h:tf;
+  yv(1) = y0;
+  
+  for i = 1:length(tv)-1
+      K1 = h*F(tv(i), yv(i));
+      K2 = h*F(tv(i) + h/2, yv(i) + K1/2);
+      K3 = h*F(tv(i) + h/2, yv(i) + K2/2);
+      K4 = h*F(tv(i) + h, yv(i) + K3);
+      yv(i+1) = yv(i) + (K1 + 2*K2 + 2*K3 + K4)/6;
+  end
+
 end
-
-#yv(1) = []; 
-disp('Valores de t: ')
-disp(tv) 
-disp('Valores de y: ')
-disp(yv) 
-#plot(tv,yv,'-o')
-#grid(gca,'minor')
-#grid on
